365109661319a06e3d6358490d3145e5d7352669
Ahsan Akhtar <ahsanakhtar91@yahoo.com>
2026-07-07 16:47:51 +0500

    A new empty project -> with React + TypeScript + Tailwind Setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className='flex items-center justify-center'>
+        <h1 className='text-2xl font-bold text-black'>
+          React + TypeScript + Tailwind Setup Complete!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
